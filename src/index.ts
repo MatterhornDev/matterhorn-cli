3c454bbb62d24a442e9d5a1ac07271ff0426bfe9
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-01-19 18:08:29 -0500

    first functional prototype

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import chalk from 'chalk'
 import * as figlet from 'figlet'
+import * as inquirer from 'inquirer'
+import * as shell from 'shelljs'
 
 const init = () => {
   console.log(
@@ -14,8 +16,54 @@ const init = () => {
   )
 }
 
+const getProjectName = (): Promise<{ project_name: string }> => {
+  return inquirer.prompt([{
+    type: 'input',
+    name: 'project_name',
+    message: 'Enter project name',
+  }])
+}
+
+const createDirectory = (project_name: string) => {
+  console.log(
+    chalk.blueBright(`Creating project directory ${project_name}`)
+  )
+  if (!shell.test('-d', project_name)) {
+    shell.mkdir(project_name)
+  } else {
+    shell.echo(chalk.red(`${project_name} already exists`))
+    shell.exit(1)
+  }
+}
+
+const gitClone = (project_name: string) => {
+  console.log(
+    chalk.blueBright(`Cloning Matterhorn project into ${project_name}`)
+  )
+  shell.exec(`git clone https://github.com/Ethan-Arrowood/matterhorn.git ${project_name}`)
+}
+
+const replace = (project_name: string) => {
+  shell.cd(project_name)
+  shell.ls('-R').forEach(fileName => {
+    if (!shell.test('-d', fileName)) {
+      shell.sed('-i', 'matterhorn', project_name, fileName)
+    }
+  })
+  shell.cd('..')
+}
+
 const run = async () => {
   init()
+
+  const { project_name } = await getProjectName()
+  createDirectory(project_name)
+  gitClone(project_name)
+  replace(project_name)
+
+  console.log(
+    chalk.green(`Your project is ready to go! Run \`cd ${project_name}\` then \`npm install\` to get started.`)
+  )
 }
 
 run()
