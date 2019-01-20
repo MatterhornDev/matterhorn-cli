88f75a97c45a2b2c3239245cae7461e57e85544b
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-01-20 16:25:29 -0500

    refactor into non-atomic flow

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import * as figlet from 'figlet'
 import * as inquirer from 'inquirer'
 import * as shell from 'shelljs'
 
-const init = () => {
+const matterhorn_cli = async () => {
+  // Intro Text
   shell.echo(
     chalk.green(
       figlet.textSync('Matterhorn CLI', {
@@ -11,74 +12,60 @@ const init = () => {
         horizontalLayout: 'default',
         verticalLayout: 'default'
       })
-    )
-  )
-  shell.echo(
+    ),
     chalk.red(
-      '⚠️  Warning ⚠️',
+      '\n⚠️  Warning ⚠️',
       '\nThis project is currently in beta.',
       '\nIt is not intended for production use.',
       '\nReport all bugs at: https://github.com/Ethan-Arrowood/matterhorn-cli/issues/new'
     )
   )
-}
 
-const getProjectName = (): Promise<{ project_name: string }> => {
-  return inquirer.prompt([{
+  // Get the users desired projectName
+  const { projectName } = await inquirer.prompt([{
     type: 'input',
-    name: 'project_name',
-    message: 'Enter project name',
+    name: 'projectName',
+    message: 'Enter project name:',
+    validate: value => !shell.test('-d', value) || 'Directory already exists.'
   }])
-}
 
-const createDirectory = (project_name: string) => {
-  shell.echo(
-    chalk.blueBright(`Creating project directory ${project_name}`)
-  )
-  if (!shell.test('-d', project_name)) {
-    shell.mkdir(project_name)
-  } else {
-    shell.echo(chalk.red(`${project_name} already exists`))
-    shell.exit(1)
-  }
-}
+  // Create directory (its guaranteed not to exist via validate function in inquirer prompt)
+  shell.echo(chalk.blueBright(`Creating project directory ${projectName}`))
+  shell.mkdir(projectName)
 
-const gitClone = (project_name: string) => {
-  shell.echo(
-    chalk.blueBright(`Cloning Matterhorn project into ${project_name}`)
-  )
-  shell.exec(`git clone https://github.com/Ethan-Arrowood/matterhorn.git ${project_name}`)
-}
+  // clone matterhorn git repo into new project dir
+  shell.echo(chalk.blueBright(`Cloning Matterhorn project into ${projectName}`))
+  let code = shell.exec(`git clone https://github.com/Ethan-Arrowood/matterhorn.git ${projectName}`).code
+  shell.echo(code === 0 ? chalk.green('Git clone successful') : chalk.red('Error: Git clone failed'))
+  code !== 0 && shell.exit(1)
 
-const removeGitDirFrom = (project_name: string) => {
-  shell.cd(project_name)
+  // Move into project directory to perform cleanup and install steps
+  shell.cd(projectName)
+
+  // remove .git directory
+  shell.echo(chalk.blueBright('Deleting .git directory'))
   shell.rm('-rf', '.git')
-  shell.cd('..')
-}
 
-const replace = (project_name: string) => {
-  shell.cd(project_name)
+  // replace 'matterhorn' with users projectName
+  shell.echo(chalk.blueBright('Replacing \'matterhorn\' with your project name'))
   shell.ls('-R').forEach(fileName => {
     if (!shell.test('-d', fileName)) {
-      shell.sed('-i', 'matterhorn', project_name, fileName)
+      shell.sed('-i', 'matterhorn', projectName, fileName)
     }
   })
+
+  // npm install dependencies
+  shell.echo(chalk.blueBright('Installing project dependencies. . .'))
+  shell.exec('npm install', { silent: true })
+
   shell.cd('..')
-}
-
-const run = async () => {
-  init()
-
-  const { project_name } = await getProjectName()
-  createDirectory(project_name)
-  gitClone(project_name)
-  removeGitDirFrom(project_name)
-  replace(project_name)
 
   shell.echo(
-    chalk.green(`Your project is ready to go! Run \`cd ${project_name}\` then \`npm install\` to get started.`)
+    chalk.green(
+      `Your project is ready to go! Run \`cd ${projectName}\` then \`npm run dev\` to get started.`
+    )
   )
 }
 
-export default run
-export { run as matterhorn_cli }
+export default matterhorn_cli
+export { matterhorn_cli }
