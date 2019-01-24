344e8d073fddb8f6d0985e9a680ecd97e93c37ef
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-01-24 17:41:27 -0500

    first implementation of --version

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,30 @@
 #!/usr/bin/env node
 import matterhorn_cli from './index'
+import * as yargsParser from 'yargs-parser'
+import * as fs from 'fs'
+import * as path from 'path'
 
-matterhorn_cli()
+const argv = yargsParser(process.argv.slice(2))
+
+const getVersion = () => {
+  fs.readFile(path.join(__dirname, '../package.json'), 'utf8', (err, data) => {
+    if (err) throw err
+
+    if (typeof data === 'string') {
+      const packageJson = JSON.parse(data)
+      console.log(packageJson.version)
+    } else {
+      console.log('im a buffer')
+    }
+  })
+}
+
+const run = () => {
+  if (argv.version) {
+    getVersion()
+  } else {
+    matterhorn_cli()
+  }
+}
+
+run()
